3f1e58b43587485b2d9e8b6ab034b242983820cf
Katy Robinson <katyorob@gmail.com>
2018-01-22 13:55:29 -0800

    added readme and updated articleView with handlebars templating at bottom

--- tama-ahmed-katy/public/scripts/articleView.js
+++ tama-ahmed-katy/public/scripts/articleView.js
@@ -134,6 +134,7 @@ var app = app || {};
   articleView.initAdminPage = () => {
     
     // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
+    var template = Handlebars.compile($('#author-stats-templare').text());
     app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
